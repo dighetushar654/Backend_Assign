f540dd7fcc5c7f5f38398b2ffde9c0b798bfbab3
Tushar Dighe <dighetushar654@gmail.com>
2021-08-02 21:09:31 +0530

    updated

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const {Schema} = require("mongoose");
 
 
 const todoSchema = new mongoose.Schema({
@@ -14,9 +16,36 @@ const todoSchema = new mongoose.Schema({
     },
     category:{
         type: String
-    }
+    },
+    User: {type: Schema.Types.ObjectId, ref: "User"}
 },
    {timestamps: true});
 
+const userSchema = new mongoose.Schema({
+    Username: {
+        type: String
+    },
+    email: {
+        type:String,
+        validate:{
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email',
+            isAsync: false
+        },
+    },
+    phone: {
+        type: String,
+        match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/,
+    },
+    role: {
+        type: String,
+    },
+    todolist: [{
+        type: Schema.Types.ObjectId, ref: "Todo"
+    }]
+},
+{timestamps: true});
+
 
 module.exports = mongoose.model('Todo', todoSchema);
+module.exports = mongoose.model('User', userSchema);
