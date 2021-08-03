83be798e50faf43ebcdbae85b542fad46aa17482
Tushar Dighe <dighetushar654@gmail.com>
2021-08-03 09:59:59 +0530

    updated

--- routes/todo.js
+++ routes/todo.js
@@ -1,21 +1,42 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
+const {check, validationResult} = require("express-validator/check");
+const auth = require("../middlewares/auth");
+
+const User = require("../models/userModel");
 const Todo = require('../models/todoModel');
 
-router.post('/', async (req, res) =>{
-    try {
-        const {name, todotitle, status, category} = req.body;
-        const newtodo = new Todo({name, todotitle, status, category});
-        const savetodo = await newtodo.save();
-        res.status(200).json(savetodo)
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({err});
-    }
+router.post('/', [auth,
+    check('name', 'name is required').not().isEmpty(),
+    check('todotitle', 'Please enter todotitle email').not().isEmpty(),
+    check('status', 'Please enter status completed or not completed').not().isEmpty(),
+    check('category', 'Enter category ex. hobby, work and task').not().isEmpty()
+        ], async (req, res) =>{
+            const errors = validationResult(req);
+                if(!errors.isEmpty()) {
+                    return res.status(400).json({errors: errors.array()})
+                }
+
+            try {
+                const {name, todotitle, status, category} = req.body;
+                const newtodo = new Todo({
+                    name,
+                    todotitle,
+                    status, 
+                    category,
+                    user: req.user.id
+                });
+                const savetodo = await newtodo.save();
+                res.status(200).json(savetodo)
+
+            } catch (err) {
+                console.log(err);
+                res.status(500).json({err});
+            }
 })
 
-router.patch('/', async (req, res) => {
+router.patch('/', auth,  async (req, res) => {
     const {name, status, todotitle, category} = req.body;
     const userExist = await Todo.findOne({name:name})
     if(userExist) {
@@ -38,7 +59,8 @@ router.get("/:id", async (req, res) => {
 
 router.get("/", async (req, res, next) => {
     try {
-        const data = await Todo.find();
+        // sorting data using createdAT field in descending order
+        const data = await Todo.find().sort({createdAt: '-1'});
         // console.log(data);
         const filters = req.query;
         const filteredCategory = data.filter(category => {
@@ -57,7 +79,7 @@ router.get("/", async (req, res, next) => {
 });
 
 
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
     const { id } = req.params;
   
     if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No Todo with id: ${id}`);
